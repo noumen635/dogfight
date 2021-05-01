9146fccb8a89109521a6f3b6dfbb2c2ded6f21a4
mr-tchounga <jordan.tchounga@2025.ucac-icam.com>
2021-05-01 21:40:26 +0100

    Phase 2

--- src/jpu2016/dogfight/model/IDogfightModel.java
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -6,7 +6,7 @@ public interface IDogfightModel {
 	
 	public IArea getArea();
 	
-	public void (Dimension dimension);
+	public void buildArea(Dimension dimension);
 	
 	public void addMoblie (IMobile Mobile);
 	
