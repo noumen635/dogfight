2cd66df07d1281be470c2377f4e7790ffd26dde9
mr-tchounga <jordan.tchounga@2025.ucac-icam.com>
2021-05-01 21:45:31 +0100

    Phase 2

---
+++ src/jpu2016/dogfight/model/IMobile.java
@@ -0,0 +1,20 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Point;
+
+public interface IMobile {
+
+	public Direction getDirection();
+	public void setDirection (Direction direction);
+	public Point getPosition();
+	public Dimension getDimension();
+	public int getWidth();
+	public int getHeight();
+	public int getSpeed();
+	public void move();
+	public void placeInArea (IArea area);
+	public boolean isPlayer (int player);
+	public void setDogfightModel(IDogfightModel dogfightModel);
+	public boolean hit();
+	public boolean isWeapon();
+}
